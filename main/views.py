4b0d851a0d7ea4b8c51675d4fd3abd3ef47f96e7
mohammedanaz <mohammedanaz@gmail.com>
2024-07-15 12:48:38 +0530

    doc strings added to views

--- main/views.py
+++ main/views.py
@@ -13,14 +13,22 @@ from django.shortcuts import get_object_or_404
 
 
 def get_tokens_for_user(user):
+    '''
+    this function creates and return an object with access and refresh token.
+    '''
     refresh = RefreshToken.for_user(user)
     return {
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
 
+
 @api_view(['POST'])
 def login_view(request):
+    '''
+    Receives data and validate the credentials with user model instance and
+    then creates tokens and serialized user data to send back.
+    '''
     user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({'detail': 'Invalid credentials.'}, status=status.HTTP_404_NOT_FOUND)
@@ -28,8 +36,14 @@ def login_view(request):
     serializer = UserSerializer(instance=user)
     return Response({'tokens': tokens, 'user': serializer.data}, status=status.HTTP_200_OK)
 
+
 @api_view(['POST'])
 def signup(request):
+    '''
+    Receives data and check for username existance. then validating the data and 
+    creates a new instance in user model. then creates tokens and serialized user data 
+    to send back. if received data not valid then response with 400 status code.
+    '''
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -38,10 +52,15 @@ def signup(request):
         return Response({'tokens': tokens, 'user': serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, JWTAuthentication])
 @permission_classes([IsAuthenticated])
 def fetch_booking(request):
+    '''
+    this view provides data from BookingData model in response to api calls. if any failure while 
+    processing data , it response with 500 status code.
+    '''
     try:
         if request.method == 'GET':
             bookings = BookingData.objects.all()
@@ -50,11 +69,16 @@ def fetch_booking(request):
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
+    
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, JWTAuthentication])
 @permission_classes([IsAuthenticated])
 def create_booking(request):
-    print('inside create booking, data is- ', request.data)
+    '''
+    this view creates new instance on BookingData model. before creating new instance
+    it converts the field names (phoneNumber, bookedDate) from received data to match the field names
+    of the model. if any failure while processing data , it response with 400 status code.
+    '''
     data = request.data.copy()
     data['phone_number'] = data.pop('phoneNumber')
     data['booked_date'] = data.pop('bookedDate')
@@ -71,4 +95,7 @@ def create_booking(request):
 @authentication_classes([SessionAuthentication, JWTAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
+    '''
+    this view is only for testing the token purpose.
+    '''
     return Response({f'passed for {request.user.username}'})
